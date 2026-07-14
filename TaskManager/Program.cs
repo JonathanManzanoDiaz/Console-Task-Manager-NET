c287231a2a5cb2330250bd59e8d650361bfb3739
Jonathan Manzano Diaz <jonathanmanzanodiaz@gmail.com>
2026-07-14 20:20:02 +0200

    feat: implement memory-based CRUD (RemoveTask, ToggleTask and GetTasks)

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -2,6 +2,19 @@
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
+            TaskManager manager = new();
+            List<TaskItem> listaDeTareas = manager.GetTasks();
+            GetTask(listaDeTareas);
+        }
+        public static void GetTask(List<TaskItem> tasks) {
+                    foreach (var task in tasks) {
+                if (task.IsCompleted == false) {
+                    Console.WriteLine($"{task.ID} | [ ] {task.Title}");
+                } else {
+                    Console.WriteLine($"{task.ID} | [\u221A] {task.Title}");
+                }
+                }
+            }
         }
     }
 }
--- TaskManager/TaskManager.cs
+++ TaskManager/TaskManager.cs
@@ -9,5 +9,43 @@ namespace TaskManager {
         public TaskManager(string filePath = "tasks.json") {
             _filePath = filePath;
         }
+        public void AddTask(string title) {
+            TaskItem newTask = new(title);
+            _tasks.Add(newTask);
+        }
+        public List<TaskItem> GetTasks() {
+            return _tasks;
+        }
+        public bool RemoveTask(int id) {
+            TaskItem taskToDelete = null;
+            foreach(var task in _tasks) {
+                // Search for task and select the one you want to delete.
+                if (task.ID == id) {
+                    taskToDelete = task;
+                    break;
+                }
+            }
+            if (taskToDelete != null) {
+                _tasks.Remove(taskToDelete);
+                return true;
+
+            }
+            return false;
+        }
+        public bool ToggleTask(int id) {
+            TaskItem taskToToggle = null;
+            foreach(var task in _tasks) {
+                if(task.ID == id) {
+                    taskToToggle = task;
+                    break;
+                }
+            }
+            if (taskToToggle != null) {
+                taskToToggle.IsCompleted = !taskToToggle.IsCompleted;
+                return true;
+            }
+            return false;
+        }
     }
+
 }
